af5e2fa93c7f68b4b6d409cc251137937b23dbc4
nirokay <80123634+nirokay@users.noreply.github.com>
2023-09-18 09:46:37 +0200

    added executable flags

--- resources.conf.sh
+++ resources.conf.sh
@@ -4,6 +4,11 @@
 # Location of this repo
 export RASPBERRY_FILES=~/raspberry
 
+# Discord bots
+export DISCORD_BOT_LOCATION=~/DiscordBots
+export DISCORD_BOTS=( Nimwit NationBot LunaLoop )
+export DISCORD_BOTS_RESTART_SECONDS=172800 # 48h
+
 # Main file, that starts all tasks
 export STARTUP_FILE="$RASPBERRY_FILES/temptasks.sh"
 
--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Import resources:
+# shellcheck disable=SC1090
 source ~/raspberry/resources.conf.sh
 
 # Run file:
+cd "$RASPBERRY_FILES" || cd ~/raspberry/ || echo "Uh oh, directory not found..."
 $STARTUP_FILE
